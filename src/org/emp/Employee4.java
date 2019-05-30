80b4944d2ca02e1cce15ab41719dc6a376278c5d
jaswanth0594 <jaswanthannamreddy@gmail.com>
2019-05-30 16:35:06 +0530

    raju

---
+++ src/org/emp/Employee4.java
@@ -0,0 +1,17 @@
+package org.emp;
+
+public class Employee4 {
+	public void empname() {
+		System.out.println("ashok");
+	}
+		public void empsalary() {
+			System.out.println("44444");
+	}
+		public static void main(String[] args) {
+			Employee4 is=new Employee4();
+			is.empname();
+			is.empsalary();
+		}
+	
+
+}
--- src/org/emp/Mapping.java
+++ src/org/emp/Mapping.java
@@ -3,7 +3,7 @@ package org.emp;
 public class Mapping {
 	public static void main(String[] args) {
 		System.out.println("bhanu");
-		System.out.println("bhanu1233");
+		System.out.println("bh
 	}
 
 }
